2f93d90f49bfcbf93d0f01a5753562a8268eb23e
ilkerburak7 <ilkerburakgulec@gmail.com>
2021-03-16 18:52:45 -0500

    New Project Update

--- src/test/java/GitTest.java
+++ src/test/java/GitTest.java
@@ -3,6 +3,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("100_000.00$");
         System.out.println("100");
+        System.out.println("Ilker");
+        System.out.println("Burak");
     }
 }
 
